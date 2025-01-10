7df6ed95b796c3543ed9d5602e454134bc1e1198
Guga Rukhadze <ruxadze0@gmail.com>
2025-01-10 15:45:30 +0400

    Added SMTP logging feature.

--- BrainstormSessions.API/Program.cs
+++ BrainstormSessions.API/Program.cs
@@ -6,36 +6,27 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Serilog
 builder.Host.UseSerilog((context, loggerConfig) =>
 {
-    loggerConfig
-        .ReadFrom.Configuration(context.Configuration) 
-        .Enrich.FromLogContext()
-        .WriteTo.Console();
+    loggerConfig.ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()                       
+        .WriteTo.Console();                            
 });
 
-// Add services to the container
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("InMemoryDb"));
-
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseInMemoryDatabase("InMemoryDb"));
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddScoped<IBrainstormSessionRepository, EFStormSessionRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    // Initialize database
     using var scope = app.Services.CreateScope();
     var repository = scope.ServiceProvider.GetRequiredService<IBrainstormSessionRepository>();
     await InitializeDatabaseAsync(repository);
 }
 
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapControllerRoute(
@@ -44,7 +35,6 @@ app.MapControllerRoute(
 
 app.Run();
 
-// Helper methods
 async Task InitializeDatabaseAsync(IBrainstormSessionRepository repo)
 {
     var sessionList = await repo.ListAsync();
